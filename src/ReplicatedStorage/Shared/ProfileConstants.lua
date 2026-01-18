5767f5755adb0672f9ce005af518bf62b9465120
JDT89 <115045921+JDT89@users.noreply.github.com>
2026-01-17 19:06:52 -0600

    Create ProfileConstants.lua

---
+++ src/ReplicatedStorage/Shared/ProfileConstants.lua
@@ -0,0 +1,16 @@
+--!strict
+
+local ProfileConstants = {}
+
+ProfileConstants.Attributes = {
+	Level = "AccountLevel",
+	Exp = "AccountExp",
+	ExpToNext = "AccountExpToNext",
+}
+
+ProfileConstants.Defaults = {
+	Level = 1,
+	Exp = 0,
+}
+
+return ProfileConstants
